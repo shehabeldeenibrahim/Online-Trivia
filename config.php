299d073e8185cf15f532ade56544315d2d31bc47
User <shehab_eldiin@hotmail.com>
2019-09-09 22:25:28 +0200

    first

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$dbHost = 'localhost';
+$dbName = 'su-trivia';
+$dbUsername = 'root';
+$dbPassword = '';
+
+$mysqli = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName); 
+?>
---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+//including the database connection file
+include("config.php");
+
+$result = mysqli_query($mysqli, "SELECT * FROM timer ORDER BY id DESC");
+while($res = mysqli_fetch_array($result)) { 
+$date = $res['date'];
+$h = $res['h'];
+$m = $res['m'];
+$s = $res['s'];
+}
+?>
+<script>
+function httpGet(theUrl)
+{
+    
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    time1 = JSON.parse(xmlHttp.responseText);
+    var time = time1.datetime;
+    index = time.indexOf("T")
+    index1 = time.indexOf(".")
+    //time = time.substr(index+1, index1-index-1);
+    time = time.substr(0, index1);
+    debugger
+    time = new Date(time);
+    
+    return time;
+}
+var time = httpGet('http://worldtimeapi.org/api/timezone/Africa/Cairo')
+var countDownDate = <?php 
+echo strtotime("$date $h:$m:$s" ) ?> * 1000;
+
+var now = time * 1000;
+// Update the count down every 1 second
+var x = setInterval(function() {
+now = now + 1000;
+// Find the distance between now an the count down date
+var distance = countDownDate - now;
+
+// Time calculations for days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+// Output the result in an element with id="demo"
+document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+minutes + "m " + seconds + "s ";
+// If the count down is over, write some text 
+if (distance < 0) {
+clearInterval(x);
+ document.getElementById("demo").innerHTML = "EXPIRED";
+}
+    
+}, 1000);
+
+    </script>
+<html>
+    <body id="demo">
+    </body>
+</html>
