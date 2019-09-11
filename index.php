d38cb74caff617efe9eea5ad4ec578a6cb3a5b51
User <shehab_eldiin@hotmail.com>
2019-09-11 20:23:28 +0200

    working with problem in delay

--- index.php
+++ index.php
@@ -35,38 +35,45 @@ function getQuestionHttp(theUrl){
     question = JSON.parse(xmlHttp.responseText);
     return question
 }
-var question = getQuestionHttp("http://localhost:8080/timer_php//getQuestion.php");
-var endTime = question.EndEpochs;
+function main()
+    {
+    var question = getQuestionHttp("http://localhost:8080/timer_php//getQuestion.php");
+    var endTime = question.EndEpochs;
 
-var time = httpGet('http://worldtimeapi.org/api/timezone/Africa/Cairo')
-var countDownDate = endTime * 1000;
+    var time = httpGet('http://worldtimeapi.org/api/timezone/Africa/Cairo')
+    var countDownDate = endTime * 1000;
 
-var now = time * 1000;
-now /= 1000
+    var now = time * 1000;
+    now /= 1000
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-now = now + 1000;
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+    now = now + 1000;
 
-// Find the distance between now an the count down date
-var distance = countDownDate - now;
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
 
-// Time calculations for days, hours, minutes and seconds
-var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-// Output the result in an element with id="demo"
-document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
-minutes + "m " + seconds + "s " + question.Question;
-// If the count down is over, write some text 
-if (distance < 0) {
-clearInterval(x);
- document.getElementById("demo").innerHTML = "EXPIRED";
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+    minutes + "m " + seconds + "s " + question.Question;
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        document.getElementById("demo").innerHTML = "Please wait for the next question";
+    clearInterval(x);
+    //  document.getElementById("demo").innerHTML = "EXPIRED";
+    main();
+    }
+        
+    }, 1000);
+    return question;
 }
-    
-}, 1000);
-
+    var apiResponse;
+    apiResponse = main();
     </script>
 <html>
     <body id="demo">
