cb9faf905d2286eb9717b72c81fcd03f9c076006
ahmedkhatter <42092979+ahmedkhatter@users.noreply.github.com>
2019-09-23 14:38:38 +0200

    New SQL file -- with answers

--- su-trivia.sql
+++ su-trivia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 23, 2019 at 12:51 AM
+-- Generation Time: Sep 23, 2019 at 02:37 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -38,17 +38,18 @@ CREATE TABLE `timer` (
   `Answer1` text NOT NULL,
   `Answer2` text NOT NULL,
   `Answer3` text NOT NULL,
-  `Answer4` text NOT NULL
+  `Answer4` text NOT NULL,
+  `CorrectAnswer` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `timer`
 --
 
-INSERT INTO `timer` (`id`, `question`, `date`, `h`, `m`, `s`, `Answer1`, `Answer2`, `Answer3`, `Answer4`) VALUES
-(1, 'name?', '2019-09-22', '19', '36', '0', 'ahmed', 'mostafa', 'Bobawy', 'Shixawy'),
-(2, 'bday?', '2019-09-22', '19', '37', '10', '1 - 1 - 2019', '2 - 1 - 2019', '3 - 1 - 2019', '4 - 1 - 2019'),
-(3, 'status?', '2019-09-22', '19', '38', '20', 'Single', 'Ready', 'To', 'Mingle');
+INSERT INTO `timer` (`id`, `question`, `date`, `h`, `m`, `s`, `Answer1`, `Answer2`, `Answer3`, `Answer4`, `CorrectAnswer`) VALUES
+(1, 'name?', '2019-09-23', '1', '30', '0', 'ahmed', 'mostafa', 'Bobawy', 'Shixawy', '1'),
+(2, 'bday?', '2019-09-23', '1', '31', '10', '1 - 1 - 2019', '2 - 1 - 2019', '3 - 1 - 2019', '4 - 1 - 2019', '2'),
+(3, 'status?', '2019-09-23', '1', '32', '20', 'Single', 'Ready', 'To', 'Mingle', '3');
 
 --
 -- Indexes for dumped tables
